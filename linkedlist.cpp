0fd985284a0b2f4fe89b69fa766c0ce2b16f520c
lalitm2005 <b23ee1089@iitj.ac.in>
2024-09-06 07:04:41 +0530

    create code for singlylinkedlist

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,45 +1,36 @@
-#include <iostream>          // Required for input/output operations
-using namespace std;        // Required for using standard library
-
-// Node structure for linked list
-struct Node {
-    int data;
-    Node* next;
+#include<bits/stdc++.h>
+using namespace std;
+  
+// Class representing a node in a singly linked list of strings
+class StringNode{
+    private:
+    string elem; // Data element stored in the node
+    StringNode* next; // Pointer to the next node in the list
+  
+    // Friend class to allow access to private members
+    friend class StringLinkedList;
 };
-
-
-// Function to create a new node
-Node* newNode(int data) {
-    Node* newNode = new Node();
-    newNode->data = data;      // Assign data to the new node        
-    newNode->next = NULL;     // Initialize next as null
-    return newNode;           // Return the new node
-}
-
-// Function to insert a new node at the beginning of the linked list
-void push(Node** head_ref, int new_data) {
-    Node* new_node = newNode(new_data);    // Create a new node with the given data
-    new_node->next = (*head_ref);          // Set the next of new node as head
-    (*head_ref) = new_node;                // Move the head to point to the new node
-}
-
-
-// Function to print the linked list
-void printList(Node* node) {
-    while (node!= NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-}
-
-// Driver code
-int main() {
-    Node* head = NULL;   // Initialize head as null
-    push(&head, 20);     // Insert 20 at the beginning of the linked list
-    push(&head, 4);      // Insert 4 at the beginning of the linked list
-    push(&head, 15);     // Insert 15 at the beginning of the linked list
-    push(&head, 85);     // Insert 85 at the beginning of the linked list
-    cout << "Original Linked List: ";
-    printList(head);     // Print the original linked list
+ 
+// Class representing a singly linked list of strings
+class StringLinkedList{
+    public:
+    // Constructor
+    StringLinkedList();
+    // Destructor
+    ~StringLinkedList();
+    // Check if the list is empty
+    bool empty() const;
+    // Get the element at the front of the list
+    const string& front() const;
+    // Add an element to the front of the list
+    void addFront(const string& e);
+    // Remove the element from the front of the list
+    void removeFront();
+    private:
+    StringNode* head; // Pointer to the head of the list
+};
+  
+// Main function
+int main(){
     return 0;
 }
