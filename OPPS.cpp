2519de9a1e5ff19c8ac63ae4843ff40190787c8c
lalitm2005 <b23ee1089@iitj.ac.in>
2024-09-06 06:04:01 +0530

    added method for class inside and outside the class

--- OPPS.cpp
+++ OPPS.cpp
@@ -5,13 +5,30 @@ class student{//class name
         int roll_no;//attribute
         string name;//attribute
         string email;//attribute
+// there are two methods of writing methods(in other words function of class)1)inside class 2)outside class
+
+// this is for inside
+        void print_name(){
+            cout<<name<<endl;
+        }
+        void print_email(){
+            cout<<email<<endl;
+        }
+        void print_roll_no();
     };
+    // this is for outside(but for somereason i have to initialize it inside the class)
+    void student::print_roll_no(){
+        cout<< roll_no<<endl;
+    }
 int main()  
 {  
     student a;
     a.roll_no=10;
     a.name="abc";
     a.email="abc.iitj.ac.in";
+    a.print_email();
+    a.print_name();
+    a.print_roll_no();
     return 0;
 
 
