a4948b38779f912edeeaec1591f418d553552f34
lalitm2005 <b23ee1089@iitj.ac.in>
2024-09-08 03:36:37 +0530

    used chatgpt to learn linkedlist

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,36 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-  
-// Class representing a node in a singly linked list of strings
-class StringNode{
+
+class Node{
+        public:
+        int data;
+        Node* next;
+        // defined a class node
+        // defining a constructor
+        Node(int val){
+            data=val;
+            next=nullptr;
+        }
+        
+    };
+class linkedlist{
     private:
-    string elem; // Data element stored in the node
-    StringNode* next; // Pointer to the next node in the list
-  
-    // Friend class to allow access to private members
-    friend class StringLinkedList;
-};
- 
-// Class representing a singly linked list of strings
-class StringLinkedList{
+    Node* head;
+    // creating a head pointer
     public:
-    // Constructor
-    StringLinkedList();
-    // Destructor
-    ~StringLinkedList();
-    // Check if the list is empty
-    bool empty() const;
-    // Get the element at the front of the list
-    const string& front() const;
-    // Add an element to the front of the list
-    void addFront(const string& e);
-    // Remove the element from the front of the list
-    void removeFront();
-    private:
-    StringNode* head; // Pointer to the head of the list
+    // creating a constructor for empty linkedlist
+    linkedlist(){
+        // this is an empty linked list with head pointing towards nullptr
+        head=nullptr;
+    }
+
 };
-  
-// Main function
+
+
 int main(){
+    
     return 0;
 }
